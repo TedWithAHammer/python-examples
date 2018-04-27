0ddb100b52c616ab771011867c900ea2fe63a725
poseidon <wangliyingwad@163.com>
2018-04-28 01:10:03 +0800

    1.获取对象信息 (dir)

--- class-test/class-test.py
+++ class-test/class-test.py
@@ -6,7 +6,48 @@
 # Description:
 # Create Time: 2018-04-28 00:44:54
 #####################################
+import types
+
+
 class Animal(object):
     pass
 
-class Dog(object)
+
+class Dog(Animal):
+    def __str__():
+        return 'this is a dog'
+    pass
+
+
+class cat(Animal):
+    def __str__():
+        return 'this is a cat'
+    pass
+
+
+def fn():
+    return 'this is a method'
+
+
+func = fn
+dog = Dog()
+# type
+print'type jjj is %s' % type('jjj')
+print'type 12 is %s' % type(12)
+print'type Dog is %s' % type(dog)
+print'type method is %s' % type(func)
+
+print'types constants func %s' % types.FunctionType
+# isinstance
+if isinstance(dog, Dog):
+    print'dog is a Dog'
+else:
+    print'dog is not a Dog'
+if isinstance(dog, Animal):
+    print'dog is Animal'
+else:
+    print'dog is not an Animal'
+
+
+# dir
+print'all infomations:%s' % dir(dog)

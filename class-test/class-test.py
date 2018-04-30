02e864d7dda2e062abcbca16d0511120001479fe
poseidon <wangliyingwad@163.com>
2018-04-30 22:25:49 +0800

    1.错误处理

--- class-test/class-test.py
+++ class-test/class-test.py
@@ -8,18 +8,49 @@
 #####################################
 import types
 
+from enum import Enum
+
+Month = Enum('Month', ('Jan', 'Feb', 'Mar', 'Apr', 'May',
+                       'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nob', 'Dec'))
+
 
 class Animal(object):
+    __slots__ = ('name', 'height')
     pass
 
 
 class Dog(Animal):
+    __slots__ = ('name')  # 无效?
+
     def __str__():
         return 'this is a dog'
+
+    def __iter__(self):
+        return self
     pass
 
 
-class cat(Animal):
+class Cat(Animal):
+    _weight = 11
+
+    @property
+    def weight(self):  # getter
+        return self._weight
+
+    @weight.setter
+    def weight(self, weight):  # setter
+        self._weight = weight
+
+    def __getattr__(self, name):
+        if name == 'name':
+            return 'not exist'
+
+    def __iter__(self):  # for in 遍历
+        return self
+
+    def __call__(self):  # derectly call
+        print'cat is calling'
+
     def __str__():
         return 'this is a cat'
     pass
@@ -31,6 +62,10 @@ def fn():
 
 func = fn
 dog = Dog()
+dog.name = 'dog'
+dog.height = 100
+cat = Cat()
+cat.weight = 200
 # type
 print'type jjj is %s' % type('jjj')
 print'type 12 is %s' % type(12)
@@ -51,3 +86,25 @@ else:
 
 # dir
 print'all infomations:%s' % dir(dog)
+
+
+print'Dog\'s height: %d' % dog.height
+
+print'Cat\'s weight: %d name:%s' % (cat.weight, cat.name)
+
+cat()
+
+cat1 = Cat()
+cat2 = Cat()
+cat3 = Cat()
+cats = [cat1, cat2, cat3]
+for n in cats:
+    print'iterate cat: %d' % n.weight
+
+
+date = Month.Jan
+#print'Enum value:%d' % date
+if date == 1:
+    print'Enum\'s name:%s' % date
+
+

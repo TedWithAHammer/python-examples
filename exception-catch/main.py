c30f317a7b71c0bae78e4f4d852d26fa47f41927
wangly1004@tpl.cntaiping.com <wangliyingwad@163.com>
2018-05-01 19:17:03 +0800

    exception catch

---
+++ exception-catch/main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+##################################
+# Author: leo
+# Description:
+# Create Time: 2018-05-01 19:15:54
+###################################
+def foo():
+
